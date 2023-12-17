8e771f1c8ada351334dbe668b8e6705bc1fc7dcc
Carlos Aragonés <c.aragones@gmail.com>
2023-12-17 21:04:58 +0100

    minor change

--- src/Delegate.h
+++ src/Delegate.h
@@ -6,6 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
 //-----------------------------------------------------------------------------
+// Last version here: https://github.com/Darky-Lucera/delegate
+//-----------------------------------------------------------------------------
 
 #include <cstdint>
 #include <cstddef>
